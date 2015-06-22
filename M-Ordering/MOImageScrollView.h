2f95bb5a12e3b857042e8b573cc9c3f225e98d30
Robben1987 <Robben@mac-down.lan>
2015-06-22 21:37:06 +0800

    update the home

---
+++ M-Ordering/MOImageScrollView.h
@@ -0,0 +1,13 @@
+//
+//  MOImageScrollView.h
+//  M-Ordering
+//
+//  Created by Li Robben on 15-6-22.
+//  Copyright (c) 2015年 Li Robben. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MOImageScrollView : UIView
+
+@end
---
+++ M-Ordering/MOImageScrollView.m
@@ -0,0 +1,21 @@
+//
+//  MOImageScrollView.m
+//  M-Ordering
+//
+//  Created by Li Robben on 15-6-22.
+//  Copyright (c) 2015年 Li Robben. All rights reserved.
+//
+
+#import "MOImageScrollView.h"
+
+@implementation MOImageScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
